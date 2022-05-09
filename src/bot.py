98dce488e0c189121d1c740b279d178aab2f1b9a
Aitzol Naberan Burgaña <anaberan@gmail.com>
2022-05-09 17:31:35 +0200

    Mezuak jasotzen dituen bot-a

--- src/bot.py
+++ src/bot.py
@@ -3,21 +3,28 @@ from dotenv import load_dotenv, find_dotenv
 import os
 load_dotenv(find_dotenv())
 
-from telegram.ext import (Updater, CommandHandler)
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters)
 
 def start(update, context):
     ''' START '''
     # Hemen gehitu komandoaren egin beharrekoa
     context.bot.send_message(update.message.chat_id, "Kaixo")
-    
+
+def mezua(update, context):
+    # prozesatu mezua
+    # https://core.telegram.org/bots/api#message
+    text = update.message.text
+
 def main():
     # Hasieraketa
-    TOKEN=os.get("TELEGRAM_TOKEN")
+    TOKEN=os.environ.get("TELEGRAM_TOKEN")
     updater=Updater(TOKEN, use_context=True)
 
     # Robotak erantzungo dituen ebentuak.
     updater.dispatcher.add_handler(CommandHandler('start',	start))
-
+    
+    # Mezu jasotzeari harpidetu
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, mezua))
     # Arrankatu bot-a. Bi funtzionamendu era polling edo webhook 
     updater.start_polling()
     # updater.start_webhook()
