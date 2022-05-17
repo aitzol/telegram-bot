4c24081fd3383442fd55d355e074dd6d74f71ca1
Aitzol Naberan Burgaña <anaberan@gmail.com>
2022-05-17 17:00:53 +0200

    Gehitu teklatua erantzuteko

--- src/bot.py
+++ src/bot.py
@@ -4,7 +4,8 @@ from ping3 import ping
 import os
 load_dotenv(find_dotenv())
 
-from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters)
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler)
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 def start(update, context):
     ''' START '''
@@ -37,8 +38,29 @@ def mezua(update, context):
     text = update.message.text
     # Hemen mezua prozesatu eta beharrezkoa bada zerbait erantzun
     if text.lower().find('errorea')!=-1:
-        context.bot.send_message(update.message.chat_id, "Mesedez jarri kontaktuan administratzailearekin")
+        keyboard = [
+            [
+                InlineKeyboardButton("Inprimagailua", callback_data="1"),
+                InlineKeyboardButton("Sarea", callback_data="2"),
+            ],
+            [InlineKeyboardButton("Beste bat", callback_data="3")],
+        ]
 
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        update.message.reply_text("Non dago errorea?: ", reply_markup=reply_markup)
+
+def botoiari_erantzun(update, context):
+    """ Hemen prozesatuko dira teklatuak bidalitako aukerak"""
+    query = update.callback_query
+    if query.data == '1':
+        msg = "Inprimagailu arduradunari abisatu diogu"
+        # Hemen benetan abisua pasa ;)
+    elif query.data == '2':
+        msg = "Sare arduradunari abisatu diogu"
+    else:
+        msg = "Monitorizazio tresnak aztertzen ari gara."
+    query.edit_message_text(msg)
+    
 def main():
     # Hasieraketa
     TOKEN=os.environ.get("TELEGRAM_TOKEN")
@@ -47,7 +69,7 @@ def main():
     # Robotak erantzungo dituen ebentuak.
     updater.dispatcher.add_handler(CommandHandler('start',	start))
     updater.dispatcher.add_handler(CommandHandler('ping', egin_ping))
-
+    updater.dispatcher.add_handler(CallbackQueryHandler(botoiari_erantzun))
     # Mezu jasotzeari harpidetu
     updater.dispatcher.add_handler(MessageHandler(Filters.text, mezua))
     # Arrankatu bot-a. Bi funtzionamendu era polling edo webhook 
